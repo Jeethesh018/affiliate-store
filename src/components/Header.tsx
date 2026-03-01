0af13b634f92da46b359fb83909bc6bb4d355d62
Jeethesh <jeethesh000@gmail.com>
2026-03-01 23:05:05 +0530

    updated code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useMemo, useState } from "react"
 import { Link } from "react-router-dom"
-import { getCategories } from "../services/productService"
 
 interface HeaderProduct {
   id: string
